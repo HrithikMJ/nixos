03092a634167ab883fa3faabf837e19e836c8ec5
HrithikMJ <hrithikmj003@gmail.com>
2024-08-16 21:05:45 +0530

    Update

--- system/postgresql/postgresql.nix
+++ system/postgresql/postgresql.nix
@@ -1,14 +1,14 @@
 { config, pkgs, ... }:
 {
-  config.services.postgresql = {
+  services.postgresql = {
     enable = true;
+    authentication = pkgs.lib.mkOverride 10 ''
+      #...
+      #type database DBuser origin-address auth-method
+      # ipv4
+      host  all      all     127.0.0.1/32   md5
+      # ipv6
+      host all       all     ::1/128        md5
+    '';
   };
-  authentication = pkgs.lib.mkOverride 10 ''
-    #...
-    #type database DBuser origin-address auth-method
-    # ipv4
-    host  all      all     127.0.0.1/32   md5
-    # ipv6
-    host all       all     ::1/128        md5
-  '';
 }
