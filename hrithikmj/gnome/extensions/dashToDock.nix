155828d9c41c618cec7021c7c1290fc086ee098a
HrithikMJ <hrithikmj003@gmail.com>
2024-08-17 22:41:32 +0530

    Fix

--- hrithikmj/gnome/extensions/dashToDock.nix
+++ hrithikmj/gnome/extensions/dashToDock.nix
@@ -3,7 +3,7 @@
 {
   dconf.settings = {
     "org/gnome/shell/extensions/dash-to-dock" = {
-      background-opacity = 0.94;
+      apply-custom-theme = true;
       running-indicator-style = "DOTS";
       transparency-mode = "FIXED";
     };
